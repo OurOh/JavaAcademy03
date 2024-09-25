4fef31f1baadf5f561f7c1cd016c1e20d4bc5c7c
OurOh <ahn980321@gmail.com>
2024-09-25 21:06:48 +0900

    0925 test

---
+++ src/a_lang/LangMain.java
@@ -0,0 +1,10 @@
+package a_lang;
+
+public class LangMain {
+
+    public static void main(String[] args) {
+
+        System.out.println("hello java");
+        System.out.println("hello java!!");
+    }
+}
